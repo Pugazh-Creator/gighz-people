c0f7fb48c4f1c5977d2d6b04ccc05bcdc39a55fa
Magendiran M. <Magendiran@gighz.com>
2025-09-18 18:29:10 +0530

    Some changes did in Employee attendance

--- app/Views/requirments/EmployeeAttendance.php
+++ app/Views/requirments/EmployeeAttendance.php
@@ -1,117 +1,152 @@
 <link rel="stylesheet" href="<?= base_url('asset/css/attendancelogs.css') ?>">
 
 
-<?php /// view('navbar/sidebar') 
-?>
-<form method="get" action="<?= site_url('employee-attendance') ?>" class="search-form emp-attendance-search-form">
-    <div class="row">
-        <div class="col-md-3">
-            <label for="month" class="form-label">Select OE Month:</label>
-            <select name="month" id="month" class="form-control">
-                <?php foreach ($months as $key => $name) : ?>
-                    <option value="<?= $key ?>" <?= ($selectedMonth == $key) ? 'selected' : '' ?>>
-                        <?= $name ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
-        </div>
+<div class="emp_attendance">
+    <div>
+        <div>
+            <form method="get" action="<?= site_url('employee-attendance') ?>" class="search-form emp-attendance-search-form">
+                <div class="row">
+                    <div class="col-md-3">
+                        <label for="month" class="form-label">Select OE Month:</label>
+                        <select name="month" id="month" class="form-control">
+                            <?php foreach ($months as $key => $name) : ?>
+                                <option value="<?= $key ?>" <?= ($selectedMonth == $key) ? 'selected' : '' ?>>
+                                    <?= $name ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
 
-        <div class="col-md-3">
-            <label for="year" class="form-label">Select Year:</label>
-            <select name="year" id="year" class="form-control">
-                <?php foreach ($years as $year) : ?>
-                    <option value="<?= $year ?>" <?= ($selectedYear == $year) ? 'selected' : '' ?>>
-                        <?= $year ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
-        </div>
+                    <div class="col-md-3">
+                        <label for="year" class="form-label">Select Year:</label>
+                        <select name="year" id="year" class="form-control">
+                            <?php foreach ($years as $year) : ?>
+                                <option value="<?= $year ?>" <?= ($selectedYear == $year) ? 'selected' : '' ?>>
+                                    <?= $year ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
 
-        <div class="col-md-3 mt-4">
-            <button type="submit" class="btn btn-primary">View</button>
+                    <div class="col-md-3 mt-4">
+                        <button type="submit" class="btn btn-primary">View</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div>
+            <div>
+                dummy data
+                <span>00:00 / 00:00</span>
+            </div>
         </div>
     </div>
-</form>
-<div class="attendance-container">
-    <div class="log-table-container employee-attendance">
+    <div class="attendance-container">
+        <div class="attendace-data">
+            <?php foreach ($attendance as $userId => $row): ?>
 
-        <table class="log-table ">
-            <thead class="log-table-head">
-                <tr>
-                    <th>Date</th>
-                    <th>Status</th>
-                    <th>Hours</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($attendance as $userId => $row): ?>
-                    <?php foreach (array_reverse($dates) as $date): ?>
-                        <tr>
-                            <td style="background :none; color:black;"><?= date('d-m-Y', strtotime($date)) ?></td>
-                            <td style="background :none; color:black;"><?= $row['records'][$date]['status'] ?></td>
-                            <td>
-                                <?php if (isset($row['records'][$date])): ?>
-                                    <?php
-                                    $timeRecord = $row['records'][$date]['total'];
-                                    ?>
-                                    <a href="#" class="punch-time" data-userid="<?= esc($userId) ?>"
-                                        data-username="<?= esc($row['name']) ?>"
-                                        data-date="<?= esc($date) ?>"
-                                        data-time="<?= esc($timeRecord) ?>">
-                                        <?= esc($timeRecord) ?>
-                                    </a>
-                                <?php else: ?>
-                                    -
-                                <?php endif; ?>
-                            </td>
-                        </tr>
+                <div class="attendance-details">
+
+                    <div>
+                        <img src="" alt="">
+                        <div class="">
+                            <h3>Present Days</h3> |
+                            <p><?= $row['presentDays'] ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="">
+                        <div class="">
+                            <h3>Compensation</h3> |
+                            <p><?= $row['compensation'] ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <h3>Leaves Not Applied</h3> |
+                            <p><?= $row['not_applied'] ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <h3>Approved Leaves</h3> |
+                            <p><?= $row['approved_leave'] ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <h3>Rejected Leaves</h3> |
+                            <p><?= $row['rejected_leave'] ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <h3>Other Saturdays</h3> |
+                            <p><?= $row['otherSaterday'] ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="" alt="">
+                        <div>
+                            <h3>Work From Home</h3> |
+                            <p><?= $row['wfh'] ?></p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h3>On Duty</h3> |
+                            <p><?= $row['od'] ?></p>
+                        </div>
+                        <div>
+                            <h3>On Duty</h3> |
+                            <p><?= $row['od'] ?></p>
+                        </div>
+                    </div>
+                </div>
+
+            <?php endforeach; ?>
+        </div>
+        <div class="log-table-container employee-attendance">
+
+            <table class="log-table ">
+                <thead class="log-table-head">
+                    <tr>
+                        <th>Date</th>
+                        <th>Status</th>
+                        <th>Hours</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($attendance as $userId => $row): ?>
+                        <?php foreach (array_reverse($dates) as $date): ?>
+                            <tr>
+                                <td style="background :none; color:black;"><?= date('d-m-Y', strtotime($date)) ?></td>
+                                <td style="background :none; color:black;"><?= $row['records'][$date]['status'] ?></td>
+                                <td>
+                                    <?php if (isset($row['records'][$date])): ?>
+                                        <?php
+                                        $timeRecord = $row['records'][$date]['total'];
+                                        ?>
+                                        <a href="#" class="punch-time" data-userid="<?= esc($userId) ?>"
+                                            data-username="<?= esc($row['name']) ?>"
+                                            data-date="<?= esc($date) ?>"
+                                            data-time="<?= esc($timeRecord) ?>">
+                                            <?= esc($timeRecord) ?>
+                                        </a>
+                                    <?php else: ?>
+                                        -
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
                     <?php endforeach; ?>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="attendace-data">
-        <?php foreach ($attendance as $userId => $row): ?>
-            <div class="employee-total-hours">
-                <h3>Total Working Hours</h3> |
-                <p><?= $row['totalOEHours'] ?></p>
-            </div>
-            <div class="attendance-details">
-                <div>
-                    <h3>Present Days</h3> |
-                    <p><?= $row['presentDays'] ?></p>
-                </div>
-                <div>
-                    <h3>Compensation</h3> |
-                    <p><?= $row['compensation'] ?></p>
-                </div>
-                <div>
-                    <h3>Leaves Not Applied</h3> |
-                    <p><?= $row['not_applied'] ?></p>
-                </div>
-                <div>
-                    <h3>Approved Leaves</h3> |
-                    <p><?= $row['approved_leave'] ?></p>
-                </div>
-                <div>
-                    <h3>Rejected Leaves</h3> |
-                    <p><?= $row['rejected_leave'] ?></p>
-                </div>
-                <div>
-                    <h3>Other Saturdays</h3> |
-                    <p><?= $row['otherSaterday'] ?></p>
-                </div>
-                <div>
-                    <h3>On Duty</h3> |
-                    <p><?= $row['od'] ?></p>
-                </div>
-                <div>
-                    <h3>Work From Home</h3> |
-                    <p><?= $row['wfh'] ?></p>
-                </div>
-            </div>
-
-        <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 <!-- Overlay -->
